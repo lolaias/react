f0745aac0ac99f9591b4f5ae29999f70a4aa1e15
lolaias <sthy1917@gmail.com>
2023-07-06 21:21:24 -0300

    prontoFinal2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import * as S from './styles'
 import truckleft from "../../assets/truckleft.png"
-import TruckRight from "../../assets/truckright.png"
+import truckright from "../../assets/truckright.png"
 
 export function Home() {
   return (
     <S.Section>
       <img src={truckleft} alt="Caminhão na esquerda" />
-      <img src={TruckRight} alt="Caminhão na direita" />
+      <img src={truckright} alt="Caminhão na direita" />
     </S.Section>
   )
 }
